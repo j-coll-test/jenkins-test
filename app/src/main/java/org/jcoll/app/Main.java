8165c1327ef90a18b0dc8c96dd0797d71d9ea3f5
Jacobo Coll Moragón <jcoll@opencb.org>
2016-08-26 09:56:23 +0100

    Multi module maven

---
+++ app/src/main/java/org/jcoll/app/Main.java
@@ -0,0 +1,14 @@
+package org.jcoll.app;
+
+import org.jcoll.lib.Utils;
+
+/**
+ * Created on 25/08/16
+ *
+ * @author Jacobo Coll &lt;jacobo167@gmail.com&gt;
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Utils().helloWorld();
+    }
+}
---
+++ lib/src/main/java/org/jcoll/lib/Utils.java
@@ -0,0 +1,14 @@
+package org.jcoll.lib;
+
+/**
+ * Created on 26/08/16
+ *
+ * @author Jacobo Coll &lt;jacobo167@gmail.com&gt;
+ */
+public class Utils {
+
+    public void helloWorld() {
+        System.out.println("Hello world!");
+    }
+
+}
