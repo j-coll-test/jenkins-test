f83cbcc3ed1fd2859aec96d6b43bd2f69462063b
Jacobo Coll Moragón <jacobo167@gmail.com>
2016-08-28 11:04:00 +0100

    New test

---
+++ app/src/test/java/org/jcoll/app/Test.java
@@ -0,0 +1,14 @@
+package org.jcoll.app;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jacobo on 28/08/16.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void testName() throws Exception {
+        assertTrue(2 > 1);
+    }
+}
